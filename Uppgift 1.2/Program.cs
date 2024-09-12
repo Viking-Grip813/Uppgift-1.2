d770a32577acf042ec482913435c5f9efb845be8
vist07001 <vist07001@E-PW05CGR8>
2024-09-12 12:48:01 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Your Pc has crashed and requires a restart");
+            Console.WriteLine("We are just collecting error info,");
+            Console.WriteLine("and then we'll restart for you.");
+        }
+    }
+}
